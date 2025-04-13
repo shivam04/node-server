68423c0c29e91820fd32e253ddb613d9511b6b07
Shivam Sinha <sinhashivam04@gmail.com>
2025-04-13 18:38:18 +0530

    node

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get('/api/posts/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log('Received Id:', id);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const globalData =  await response.json();
+    res.json({ message: 'Data received successfully!', globalData });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://127.0.0.1:${PORT}`);
+});
